52e255c42e580ec5db6264dd6fdfadabca1312b4
Demo User <debian@beaglebone.localdomain>
2017-10-15 22:42:53 +0000

    hw06 upload

---
+++ hw06/ReadMe.txt
@@ -0,0 +1,20 @@
+Daniel Neelappa Display SPI interface homework
+
+I moved the files over this directory from Dr. Yoder's main repo.
+
+I modified the code to expand the drawing width and select multiple colors.
+During use, I wanted it to seem like waves in the ocean. Instead of drawing
+a wide box for the cursor, it will create a triangle like shape. This is made
+using a single for loop, while a box would most likely take two for loops to
+draw. There is a delay inside for the color fliping and the wave behavior. 
+The delay is updated each time the drawing occurs. After an interval, the color
+flips between white to blue or visa-versa. Also, the width grows until it reaches that interval and then goes back to a width of 1 pixel.
+This way, it looks some-what like a wave crashing against the shore. I wanted
+to make a fading look, however, I'm not sure how to fade the current color,
+let's say blue, to another (black). I thought that would be interesting to have,
+but I don't have the computer graphics knowledge to make that.
+
+The display runs off of GP0 and SPi1 ports on the beaglebone blue.
+
+To run the program use sudo ./etch-a-sketch
+if the exec is not there, then use "make" to create it.
---
+++ hw06/pygame/weatherStation.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# // Displays temp and humidity and forcast from wunderground
+# // 
+# // Gets data from weather underground.
+# // Displays as
+# //  Temp:
+# //  Hum:
+# //  lo:
+# //  hi:
+# //      time
+# // Where Temp is the outdoor temp from wunderground and lo and hi are the 
+# //  forecasted low and hi temps.
+
+import requests
+
+# Get outdoor temp and forcast from wunderground
+urlWeather = "http://api.wunderground.com/api/ec7eb641373d9256/history/conditions/forecast/q/IN/Brazil.json"
+r = requests.get(urlWeather)
+if(r.status_code==200):
+    # print("headers: ", r.headers)
+    # print("text: ", r.text)
+    # print("json: ", r.json())
+    weather = r.json()
+    print("Temp: ", weather['current_observation']['temp_f'])
+    print("Yesterday Max: ", weather['history']['dailysummary'][0]['maxtempi'])
+    print("Yesterday Min: ", weather['history']['dailysummary'][0]['mintempi'])
+    print("Humid:", weather['current_observation']['relative_humidity'])
+    print("Low:  ", weather['forecast']['simpleforecast']['forecastday'][0]['low']['fahrenheit'])
+    print("High: ", weather['forecast']['simpleforecast']['forecastday'][0]['high']['fahrenheit'])
+#    print("All : ", weather['forecast']['simpleforecast']['forecastday'])
+else:
+    print("status_code: ", r.status_code)
